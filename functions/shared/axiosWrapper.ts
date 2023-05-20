d7865db817f25af26308f1be01d9cc7ad53fd96c
Takeru KIDO <keidy2463@gmail.com>
2023-05-20 18:37:29 +0900

    Delete functions:build npm script

--- functions/shared/axiosWrapper.ts
+++ functions/shared/axiosWrapper.ts
@@ -7,15 +7,73 @@ import {
   DeepLTranslation,
 } from "../../types/translation";
 
-const DEEPL_URL: string = "https://api-free.deepl.com/v2/translate";
-const COGNITIVE_URL: string =
+const AZURE_TRANSLATOR_URL: string =
   "https://api.cognitive.microsofttranslator.com/translate";
+const DEEPL_URL: string = "https://api-free.deepl.com/v2/translate";
 
 /**
- * 指定した英語の文字列群をDeepL翻訳でそれぞれ日本語に翻訳する
+ * 指定した英語の文字列群をAzure Translatorでそれぞれ日本語に翻訳する
  * @param {string[]} texts 英語の文字列群
- * @returns {Promise<string[] | undefined>} 日本語に翻訳した文字列群(文字列上限に達した場合はundefined)
- * @throws 文字列上限以外のエラーがスローされた場合
+ * @returns {Promise<string[] | undefined>} 日本語に翻訳した文字列群(Azure Translatorの無料枠を使い切った場合はundefined)
+ * @throws Azure Translatorの無料枠を使い切った以外のエラーがスローされた場合
+ */
+export const translateByAzureTranslator = async (
+  texts: string[]
+): Promise<string[] | undefined> => {
+  if (texts.length === 0) return [];
+
+  const cognitiveKey: string | undefined = process.env["COGNITIVE_KEY"];
+  if (!cognitiveKey) {
+    throw new Error("Unset COGNITIVE_KEY");
+  }
+
+  try {
+    const res: AxiosResponse<CognitiveResponse, any> = await axios.post<
+      CognitiveResponse,
+      AxiosResponse<CognitiveResponse>,
+      { Text: string }[]
+    >(
+      AZURE_TRANSLATOR_URL,
+      texts.map((text: string) => {
+        return { Text: text };
+      }),
+      {
+        headers: {
+          "Ocp-Apim-Subscription-Key": cognitiveKey,
+          "Ocp-Apim-Subscription-Region": "japaneast",
+          "Content-Type": "application/json",
+        },
+        params: {
+          "api-version": "3.0",
+          from: "en",
+          to: ["ja"],
+        },
+        responseType: "json",
+      }
+    );
+    return res.data.map(
+      (cognitiveTranslation: CognitiveTranslation) =>
+        cognitiveTranslation.translations[0].text
+    );
+  } catch (error) {
+    if (
+      axios.isAxiosError(error) &&
+      error.response &&
+      error.response.status === 403
+    ) {
+      // Azure Translatorの無料枠を使い切った場合は403エラーとなる
+      return undefined;
+    } else {
+      throw error;
+    }
+  }
+};
+
+/**
+ * 指定した英語の文字列群をDeepLでそれぞれ日本語に翻訳する
+ * @param {string[]} texts 英語の文字列群
+ * @returns {Promise<string[] | undefined>} 日本語に翻訳した文字列群(DeepLの無料枠を使い切った場合はundefined)
+ * @throws DeepLの無料枠を使い切った以外のエラーがスローされた場合
  */
 export const translateByDeepL = async (
   texts: string[]
@@ -49,55 +107,10 @@ export const translateByDeepL = async (
       error.response &&
       error.response.status === 456
     ) {
-      // DeepL無料枠の上限500000文字を超過した場合は456エラーとなる
+      // DeepLの無料枠を使い切った場合は456エラーとなる
       return undefined;
     } else {
       throw error;
     }
   }
-};
-
-/**
- * 指定した英語の文字列群をAzure Translatorでそれぞれ日本語に翻訳する
- * @param {string[]} texts 英語の文字列群
- * @returns {Promise<string[]>} 画像URLフラグ、翻訳エスケープフラグが共にOFFの場合のみ日本語に翻訳した文字列群
- * @throws エラーがスローされた場合
- */
-export const translateByCognitive = async (
-  texts: string[]
-): Promise<string[]> => {
-  if (texts.length === 0) return [];
-
-  const cognitiveKey: string | undefined = process.env["COGNITIVE_KEY"];
-  if (!cognitiveKey) {
-    throw new Error("Unset COGNITIVE_KEY");
-  }
-
-  const cognitiveRes: AxiosResponse<CognitiveResponse, any> = await axios.post<
-    CognitiveResponse,
-    AxiosResponse<CognitiveResponse>,
-    { Text: string }[]
-  >(
-    COGNITIVE_URL,
-    texts.map((text: string) => {
-      return { Text: text };
-    }),
-    {
-      headers: {
-        "Ocp-Apim-Subscription-Key": cognitiveKey,
-        "Ocp-Apim-Subscription-Region": "japaneast",
-        "Content-Type": "application/json",
-      },
-      params: {
-        "api-version": "3.0",
-        from: "en",
-        to: ["ja"],
-      },
-      responseType: "json",
-    }
-  );
-  return cognitiveRes.data.map(
-    (cognitiveTranslation: CognitiveTranslation) =>
-      cognitiveTranslation.translations[0].text
-  );
 };
