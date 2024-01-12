b0ea99113b584e6711132544aab4dc6b6e1dafa6
Takeru KIDO <keidy2463@gmail.com>
2024-01-12 21:40:08 +0900

    Debug

--- functions/src/importItems.ts
+++ functions/src/importItems.ts
@@ -1,7 +1,7 @@
 import { FunctionResult, InvocationContext } from "@azure/functions";
-import { FeedResponse, ItemResponse, SqlQuerySpec } from "@azure/cosmos";
+import { FeedResponse, SqlQuerySpec } from "@azure/cosmos";
 import deepEqual from "fast-deep-equal";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 import { ImportItem } from "../import";
 import {
   EscapeTranslatedIdxes,
@@ -9,7 +9,7 @@ import {
   Question,
   Test,
 } from "../cosmosDB";
-import { getReadOnlyContainer, getReadWriteContainer } from "./cosmosDBWrapper";
+import { getReadOnlyContainer } from "./cosmosDBWrapper";
 
 const COSMOS_DB_DATABASE_NAME = "Users";
 const COSMOS_DB_CONTAINER_NAMES = { test: "Test", question: "Question" };
@@ -19,13 +19,14 @@ export default async function (
   context: InvocationContext
 ): Promise<FunctionResult> {
   try {
-    // TODO: courseName、testName、blobの中身を取得
-    context.info({ blob });
-    const courseName: string = (blob as any).courseName;
-    const testName: string = (blob as any).testName;
+    context.info({ context });
+    context.info({ triggerMetadata: context.triggerMetadata });
+    const courseName: string = context.triggerMetadata?.courseName as string;
+    const testName: string = context.triggerMetadata?.testName as string;
     context.info({ courseName, testName });
 
     // Blobトリガーで受け取ったjsonファイルのバイナリデータをImportItem[]型として読込み
+    context.info({ blobType: typeof blob });
     const jsonData: ImportItem[] = JSON.parse(blob as any);
 
     // UsersテータベースのTestコンテナーの項目を取得
@@ -50,51 +51,51 @@ export default async function (
     }
 
     // 取得したUsersテータベースのTestコンテナーの項目が存在し差分がない場合以外はupsert
-    let testId: string;
-    if (
-      insertedTestItems.length === 0 ||
-      (insertedTestItems.length === 1 &&
-        insertedTestItems[0].length !== jsonData.length)
-    ) {
-      testId =
-        insertedTestItems.length === 0 ? uuidv4() : insertedTestItems[0].id;
-      const upsertTestItem: Test = {
-        courseName,
-        testName,
-        id: testId,
-        length: jsonData.length,
-      };
-      context.info({ upsertTestItem });
-      const res: ItemResponse<Test> = await getReadWriteContainer(
-        COSMOS_DB_DATABASE_NAME,
-        COSMOS_DB_CONTAINER_NAMES.test
-      ).items.upsert<Test>(upsertTestItem);
-      if (res.statusCode >= 400) {
-        throw new Error(
-          `Status Code ${res.statusCode}: ${JSON.stringify(upsertTestItem)}`
-        );
-      }
-    } else {
-      testId = insertedTestItems[0].id;
-    }
+    // let testId: string;
+    // if (
+    //   insertedTestItems.length === 0 ||
+    //   (insertedTestItems.length === 1 &&
+    //     insertedTestItems[0].length !== jsonData.length)
+    // ) {
+    //   testId =
+    //     insertedTestItems.length === 0 ? uuidv4() : insertedTestItems[0].id;
+    //   const upsertTestItem: Test = {
+    //     courseName,
+    //     testName,
+    //     id: testId,
+    //     length: jsonData.length,
+    //   };
+    //   context.info({ upsertTestItem });
+    //   const res: ItemResponse<Test> = await getReadWriteContainer(
+    //     COSMOS_DB_DATABASE_NAME,
+    //     COSMOS_DB_CONTAINER_NAMES.test
+    //   ).items.upsert<Test>(upsertTestItem);
+    //   if (res.statusCode >= 400) {
+    //     throw new Error(
+    //       `Status Code ${res.statusCode}: ${JSON.stringify(upsertTestItem)}`
+    //     );
+    //   }
+    // } else {
+    //   testId = insertedTestItems[0].id;
+    // }
 
     // UsersテータベースのQuestionコンテナーの項目を全取得
     let insertedQuestionItems: Question[] = [];
-    if (insertedTestItems.length > 0) {
-      // UsersテータベースのTestコンテナーの項目が取得できた場合のみクエリを実行
-      const questionQuery: SqlQuerySpec = {
-        query: "SELECT * FROM c WHERE c.testId = @testId",
-        parameters: [{ name: "@testId", value: testId }],
-      };
-      const insertedQuestionItemsRes: FeedResponse<Question> =
-        await getReadOnlyContainer(
-          COSMOS_DB_DATABASE_NAME,
-          COSMOS_DB_CONTAINER_NAMES.question
-        )
-          .items.query<Question>(questionQuery)
-          .fetchAll();
-      insertedQuestionItems = insertedQuestionItemsRes.resources;
-    }
+    // if (insertedTestItems.length > 0) {
+    //   // UsersテータベースのTestコンテナーの項目が取得できた場合のみクエリを実行
+    //   const questionQuery: SqlQuerySpec = {
+    //     query: "SELECT * FROM c WHERE c.testId = @testId",
+    //     parameters: [{ name: "@testId", value: testId }],
+    //   };
+    //   const insertedQuestionItemsRes: FeedResponse<Question> =
+    //     await getReadOnlyContainer(
+    //       COSMOS_DB_DATABASE_NAME,
+    //       COSMOS_DB_CONTAINER_NAMES.question
+    //     )
+    //       .items.query<Question>(questionQuery)
+    //       .fetchAll();
+    //   insertedQuestionItems = insertedQuestionItemsRes.resources;
+    // }
 
     // 読み込んだjsonファイルの各ImportItemにて、取得したUsersテータベースの
     // Questionコンテナーに存在して差分がない項目を抽出
@@ -172,30 +173,30 @@ export default async function (
     // 暗号化したUsersテータベースのQuestionコンテナーの各項目をupsert
     // 比較的要求ユニット(RU)数が多いDB操作を行うため、upsertの合間に3秒間sleepする
     // https://docs.microsoft.com/ja-jp/azure/cosmos-db/sql/troubleshoot-request-rate-too-large
-    if (upsertImportItems.length > 0) {
-      const sleep = (sleepPeriod: number): Promise<unknown> =>
-        new Promise((resolve) => setTimeout(resolve, sleepPeriod));
+    // if (upsertImportItems.length > 0) {
+    //   const sleep = (sleepPeriod: number): Promise<unknown> =>
+    //     new Promise((resolve) => setTimeout(resolve, sleepPeriod));
 
-      for (let idx = 0; idx < upsertImportItems.length; idx++) {
-        const item: Question = {
-          ...upsertImportItems[idx],
-          id: `${testId}_${idx + 1}`,
-          number: idx + 1,
-          testId,
-        };
-        const res: ItemResponse<Question> = await getReadWriteContainer(
-          COSMOS_DB_DATABASE_NAME,
-          COSMOS_DB_CONTAINER_NAMES.question
-        ).items.upsert<Question>(item);
-        if (res.statusCode >= 400) {
-          throw new Error(
-            `Status Code ${res.statusCode}: ${JSON.stringify(item)}`
-          );
-        }
+    //   for (let idx = 0; idx < upsertImportItems.length; idx++) {
+    //     const item: Question = {
+    //       ...upsertImportItems[idx],
+    //       id: `${testId}_${idx + 1}`,
+    //       number: idx + 1,
+    //       testId,
+    //     };
+    //     const res: ItemResponse<Question> = await getReadWriteContainer(
+    //       COSMOS_DB_DATABASE_NAME,
+    //       COSMOS_DB_CONTAINER_NAMES.question
+    //     ).items.upsert<Question>(item);
+    //     if (res.statusCode >= 400) {
+    //       throw new Error(
+    //         `Status Code ${res.statusCode}: ${JSON.stringify(item)}`
+    //       );
+    //     }
 
-        await sleep(3000);
-      }
-    }
+    //     await sleep(3000);
+    //   }
+    // }
 
     return {
       status: 200,
